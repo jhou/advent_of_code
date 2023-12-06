70b03b69e4d54cd0909b348b8ab69db49705def1
Jeremy Hou <jeremy.hou@checkr.com>
2023-12-05 22:38:48 -0800

    day 6

--- 2023/day6.js
+++ 2023/day6.js
@@ -4,18 +4,33 @@ input = input.trimEnd();
 const jcl = (sss) => console.log(JSON.stringify(sss));
 const cl = console.log;
 
-const msgSize = 14;
-const testPos = (pos) => {
-  let testSet = new Set();
-  for (let i = 1; i <= msgSize; i++) {
-    testSet.add(input[pos - i]);
-  }
-  return testSet.size === msgSize;
-};
+const findWays = (times, distances) => {
+  let allWaysToWin = [];
+  for (let i = 0; i < times.length; i++) {
+    let t = times[i],
+        d = distances[i]
+        curWaysToWin = 0;
 
-for (let i = msgSize; i < input.length; i++) {
-  if (testPos(i)) {
-    cl(i);
-    break;
+    for (let j = 1; j < t - 1; j++) {
+      if (j * (t - j) > d) {
+        curWaysToWin++;
+      }
+    }
+    allWaysToWin.push(curWaysToWin);
   }
+  return allWaysToWin;
 }
+
+let [times, distances] =  input.split('\n');
+times = times.split(': ')[1].replaceAll(/\s+/g, ' ').trim().split(' ');
+distances = distances.split(': ')[1].replaceAll(/\s+/g, ' ').trim().split(' ');
+let part1WaysToWin = findWays(times, distances);
+
+cl(`Part 1 - Margin: ${part1WaysToWin.reduce(Math.imul, 1)}`);
+
+let [times2, distances2] =  input.split('\n');
+times2 = [BigInt(parseInt(times2.split(': ')[1].replaceAll(/\s+/g, ''), 10))];
+distances2 = [BigInt(parseInt(distances2.split(': ')[1].replaceAll(/\s+/g, ''), 10))];
+let part2WaysToWin = findWays(times2, distances2);
+
+cl(`Part 2 - ways to win: ${part2WaysToWin[0]}`);
